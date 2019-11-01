29b76584ebc55dcc50f901e8129505c48da80672
nitik goyal <44530112+nitikgoyal@users.noreply.github.com>
2019-11-01 21:39:28 +0530

    Queue_menudriven
    java git

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,71 +1,111 @@
-class QNode 
-{ 
-    int key; 
-    QNode next; 
-  
-    
-    public QNode(int key) 
-    { 
-        this.key = key; 
-        this.next = null; 
-    } 
-} 
-  
 
-class Queue { 
-    QNode front, rear; 
-  
-    public Queue() 
-    { 
-        this.front = this.rear = null; 
-    } 
-  
-    
-    void enqueue(int key) 
-    { 
-  
-        
-        QNode temp = new QNode(key); 
-  
-       
-        if (this.rear == null) { 
-            this.front = this.rear = temp; 
-            return; 
-        } 
-  
-       
-        this.rear.next = temp; 
-        this.rear = temp; 
-    } 
-  
-    
-    QNode dequeue() 
-    { 
-        
-        if (this.front == null) 
-            return null;         
-        QNode temp = this.front; 
-        this.front = this.front.next; 
-  
-        
-        if (this.front == null) 
-            this.rear = null; 
-        return temp; 
-    } 
+import java.util.Scanner;
 
-    public static void main(String[] args) 
-    { 
-        Queue q = new Queue(); 
-        q.enqueue(10); 
-        q.enqueue(20); 
-        q.dequeue(); 
-        q.dequeue(); 
-        q.enqueue(30); 
-        q.enqueue(40); 
-        q.enqueue(50); 
-  
-        System.out.println("Dequeued item is " + q.dequeue().key); 
-        System.out.println("Dequeued item is " + q.dequeue().key); 
-        System.out.println("Dequeued item is " + q.dequeue().key); 
-    } 
-} 
+public class Queue {
+		Node front,rear;
+		static class Node
+		{
+			int data;
+			Node next;
+			Node(int d)
+			{
+				data=d;
+			}
+		}
+		public static void enQueue(Queue q, int data)
+		{
+			Node newNode=new Node(data);
+			newNode.next=null;
+			if(q.front == null)
+			{
+			q.front=newNode;
+			q.rear=newNode;
+			System.out.println(data+" is inserted");
+			}
+			else
+			{
+				Node last=q.front;
+				while(last.next!=null)
+				{
+					last=last.next;
+				}
+				last.next=newNode;
+				q.rear=newNode;
+				System.out.println(data+" is inserted");
+			}
+		
+		}
+		public void deQueue()
+		{
+			if(front==null)
+			{
+				System.out.println("Queue is empty");
+			}
+			else
+			{
+			Node temp=front;
+			System.out.println(temp.data+" is deleted");
+			front=temp.next;
+			}
+		}
+
+		public void display()
+		{
+			Node temp=front;
+			System.out.println("Queue is:");
+			while(temp!=null)
+			{
+				System.out.print(temp.data+" ");
+				temp=temp.next;
+			}
+			System.out.println();
+		}
+
+	public static void main(String[] args) {
+		Queue q=new Queue();
+		Scanner sc=new Scanner(System.in);
+		String option1, option2;
+		option2 = "yes";
+		while (option2.equals("yes")) {
+			System.out.println("Enter 1 to insert the element in queue");
+			System.out.println("Enter 2 to delete in queue");
+			System.out.println("Enter 3 to display");
+			option1 = sc.next();
+			switch (option1) {
+			case "1":
+				System.out.println("Enter the element:");
+				int num1 = sc.nextInt();
+				q.enQueue(q,num1);
+				break;
+			case "2":
+				q.deQueue();
+				break;
+			case "3":
+				q.display();
+				break;
+			default:
+				System.out.println("Invalid Input");
+
+			}
+			System.out.println("Whether you want to continue,enter yes or no");
+			option2 = sc.next();
+			if (option2.equals("yes") || option2.equals("no")) {
+				continue;
+			} 
+			else {
+				System.out.println("Enter valid choice:");
+				while (!option2.equals("yes")) {
+					option2 = sc.next();
+					if (option2.equals("no"))
+						break;
+					else
+						System.out.println("Enter valid choice:");
+				}
+
+			}
+
+		}
+		System.out.println("**PROGRAM FINISHED**");
+	}
+
+}
